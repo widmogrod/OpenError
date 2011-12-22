b6e7403f37311831f3a172f847053baccb51c051
Gabriel <widmogrod@gmail.com>
2011-12-22 08:39:00 +0100

    first API draft

--- public/script/openerror.js
+++ public/script/openerror.js
@@ -7,10 +7,73 @@
         this.options = {};
     }
     
+    /*
+        Bootstrap
+    */
+    
     OpenError.prototype.init = function()
     {
         
     }
     
+    /*
+        Model
+    */  
+    
+    OpenError.prototype.addIssue = function()
+    {
+        
+    }
+    
+    OpenError.prototype.getListOfIssues = function()
+    {
+        
+    }
+    
+    OpenError.prototype.getUserData = function()
+    {
+        
+    }
+    
+    /*
+        Controller
+    */
+    
+    OpenError.prototype.openImageEditor = function()
+    {
+        
+    }
+    
+    OpenError.prototype.closeImageEditor = function()
+    {
+        
+    }
+
+    OpenError.prototype.openDrawer = function()
+    {
+        
+    }
+    
+    OpenError.prototype.closeDrawer = function()
+    {
+        
+    }
+
+    OpenError.prototype.createSnapshot = function()
+    {
+        var configuration = {
+            logging: false,
+            ready: function(renderer) {
+                document.body.appendChild(renderer.canvas);
+                
+                var canvas = $(renderer.canvas);
+                canvas.css('margin-top','500px');
+                
+                console.log(renderer);
+            }
+        };
+    
+        new html2canvas($('body').get(0), configuration);
+    }
     
 }) (jQuery, window);
